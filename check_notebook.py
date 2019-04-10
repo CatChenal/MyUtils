893996d5a6d1a023b9449aaa3ac9fbd5a3e7a551
Cat Chenal <catchenal@gmail.com>
2019-04-10 09:18:43 -0400

    check_notebook update

--- check_notebook.py
+++ check_notebook.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 
-def check_book():
-    """
-    Utils to check a Jupyter notebook environment:
-    a markdown cell in a jupyter lab notebook cannot render
-    variables: the cell text needs to be created with
-    IPython.display.Markdown.
-    """
-    def is_lab_notebook():
+def is_lab_notebook():
         import re
         import psutil
         
         return any(re.search('jupyter-lab-script', x)
                    for x in psutil.Process().parent().cmdline())
+                   
+def check_notebook():
+    """
+    Util to check a Jupyter notebook environment:
+    a markdown cell in a jupyter lab notebook cannot render
+    variables: the cell text needs to be created with
+    IPython.display.Markdown.
+    """
 
     if is_lab_notebook():
         # need to use Markdown if referencing variables:
         from IPython.display import Markdown
-        msg = "This is a JupyterLab notebook: Use \
-               `IPython.display.Markdown()` if referencing variables."
+               
+        msg = "This is a <span style=\"color:red;\">JupyterLab notebook \
+              </span>: Use `IPython.display.Markdown()` if referencing variables; \
+              {{var}} does not work."
         return Markdown('### {}'.format(msg))
